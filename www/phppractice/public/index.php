5fd7f61395068aac35f346923f3db865e845095e
JUN <soipon0@gmail.com>
2018-07-18 12:36:19 +0900

    ＃02はじめてのPHPプログラム

--- www/phppractice/public/index.php
+++ www/phppractice/public/index.php
@@ -1,3 +1,14 @@
 <?php
 
-echo "iwamura san!!";
+
+
+echo "hello from the TOP!!";
+ ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<body>
+  <p>Hello World <?php echo "from php"; ?></p>
+</body>
+
+</html>
