47fdda3e2dc6ccae63542d87e4339e91043e6a26
JUN <soipon0@gmail.com>
2018-07-24 12:39:41 +0900

    中身の変更

--- www/phppractice/public/index.php
+++ www/phppractice/public/index.php
@@ -1,14 +1,5 @@
 <?php
 
 
-
-echo "hello from the TOP!!";
- ?>
-
-<!DOCTYPE html>
-<html lang="ja">
-<body>
-  <p>Hello World <?php echo "from php"; ?></p>
-</body>
-
-</html>
+$msg = "hello from the TOP!";
+echo $msg;
