9874e6f37b24c8db39ea38b1bc6b72ab76f89bb5
JUN <soipon0@gmail.com>
2018-11-06 22:17:25 +0900

    #07 文字列を扱ってみよう

--- www/phppractice/public/index.php
+++ www/phppractice/public/index.php
@@ -1,24 +1,21 @@
 <?php
 
-//数値型の演算
+// 文字列
 
-// $x = 10 % 3; //1
-// $y = 30.2 / 4; //7.55
-// var_dump($x);
-// var_dump($y);
+//　""特殊文字(\n,\t) 変数
+// '' 
 
 
-//単項演算子　＋＋　ーー
+// $name = "taguchi";
+// $s1 = "hello $name!\nhello again!";
+// $s1 = "hello {$name}!\nhello again!"; 
+// $s1 = "hello ${name}!\nhello again!"; 
+// $s2 = 'hello $name!\nhello again!'; 
 
-// $z = 5;
-// $z++; //6
-// var_dump($z);
-// $z--; //5
-// var_dump($z);
+//var_dump($s1);
+//var_dump($s2);
 
-//代入を伴う演算子
 
-$x = 5;
-// $x = $x + 2;
-$x *= 2; //10
-var_dump($x);
+// 連結
+$s = "hello" . "world";
+var_dump($s);
