4c82fb00a1d57d69733b57ea5029da8f5e691446
JUN <soipon0@gmail.com>
2019-04-20 19:56:30 +0900

    whereを使ってレコードを抽出

--- www/phppractice/public/myqpp.sql
+++ www/phppractice/public/myqpp.sql
@@ -1,21 +1,29 @@
 drop table if exists users;
 create table users (
   id int unsigned primary key auto_increment,
-  name varchar(20) unique,
+  name varchar(20),
   -- score float not null
-  score float default 0.0
+  score float
 );
--- alter table users add column email varchar(255);
-alter table users add column email varchar(255) after name;
--- こうするとnameの後に挿入される
--- カラム削除はdrop column カラム名;
-alter table users drop column score;
--- データ型を変更したい時は alter table users changeを使えばOK
-alter table users name user_name varchar(80) default 'nobody';
 
-desc users;
+insert into users (name, score) values ('taguchi', 5.8);
+insert into users (name, score) values ('fkoji', 8.2);
+insert into users (name, score) values ('dotinstall', 6.1);
+insert into users (name, score) values ('Tanaka', 4.2);
+insert into users (name, score) values ('yamada', null);
+insert into users (name, score) values ('tashiro', 7.9);
 
--- テーブル名の変更
-alter table users rename persons;
-show tables;
-drop table if exists persons;
+-- select * from users;
+select id, name from users;
+-- < > <= >= = <> != (<> !=同じ意味)
+-- is null, is not null
+-- and or not
+-- where に使える条件文
+-- select * from users where score >= 6.0;
+-- select * from users where score >= 3.0 and score <= 6.0;
+-- select * from users where score between 3.0 and 6.0;
+-- どちらも同じ意味である
+
+-- select * from users where name = 'taguchi' or name = 'fkoji';
+-- select * from users where name in ('taguchi', 'fkoji');
+-- これも同じ意味 nameがtaguchiさんかfkojiさんをさがしてきてってこと
